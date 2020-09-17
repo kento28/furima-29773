b0f5d4140042597d8971498c05cf674687da4710
kento28 <atnorisiro@gmail.com>
2020-09-17 20:38:32 +0900

    マイグレーションの実行

--- db/migrate/20200917102939_create_items.rb
+++ db/migrate/20200917102939_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.references :user,     null: false, foreign_key: true
+      t.string     :name,     null: false
+      t.text       :text,     null: false
+      t.integer    :category, null: false
+      t.integer    :state,    null: false
+      t.integer    :postage,  null: false
+      t.integer    :region,   null: false
+      t.integer    :shipping_days, null: false
+      t.integer    :price,    null: false
 
       t.timestamps
     end
