9743776462b388b18c8a4e0c53eb9d3225ddfcec
kento28 <atnorisiro@gmail.com>
2020-09-18 15:52:14 +0900

    item.rb バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  with_options presence: true do
+    validates: name
+    validates: text
+    validates: category, numericality: { other_than: 1, message: 'Select' }
+    validates: state,    numericality: { other_than: 1, message: 'Select' }
+    validates: postage,  numericality: { other_than: 1, message: 'Select' }
+    validates: region,   numericality: { other_than: 1, message: 'Select' }
+    validates: shipping_days, numericality: { other_than: 1, message: 'Select' }
+    validates: price,    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+
+  end
 end
