01923d784577d6c586ede878404714daece54fa5
kento28 <atnorisiro@gmail.com>
2020-09-23 13:18:07 +0900

    ルボコップ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,12 +11,12 @@ class Item < ApplicationRecord
     validates :name
     validates :text
 
-    with_options  numericality: { other_than: 1, message: 'Select' } do
-    validates :category_id 
-    validates :condition_id
-    validates :postage_id
-    validates :shipping_days_id
-  end
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates :condition_id
+      validates :postage_id
+      validates :shipping_days_id
+    end
 
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select' }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
