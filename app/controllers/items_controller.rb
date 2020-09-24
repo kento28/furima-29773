f2ef51447e580e5b286d820c56994ed2d357c8c6
kento28 <atnorisiro@gmail.com>
2020-09-24 17:51:26 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_item, only: [:show, :edit]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -21,11 +23,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
   def item_params
     params.require(:item).permit(:image, :name, :category_id, :text, :condition_id, :price, :postage_id, :shipping_day_id, :prefecture_id).merge(user_id: current_user.id)
   end
