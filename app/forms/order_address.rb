1613d5e246f756edc12dca32fc1b11abe0892543
kento28 <atnorisiro@gmail.com>
2020-10-01 17:15:33 +0900

    Update order_address.rb

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal, :city, :prefecture_id, :address1, :address2, :telephone, :item_id, :user_id
+  attr_accessor :postal, :city, :prefecture_id, :address1, :address2, :telephone, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/ }
@@ -10,14 +10,13 @@ class OrderAddress
     validates :telephone, format: { with: /\A\d{11}\z/ , message: 'は11桁ハイフンなしで入力してください。' }
     validates :user_id
     validates :item_id
+    validates :token
 
   end
 
   def save
-    item = Item.find(item_id)
-    user = User.find(user_id)
-    Address.create(postal: postal, prefecture_id: prefecture_id, city: city, address1: address1,
-                          address2: address2, telephone: telephone, item_id: item.id, user_id: user.id)
-    Order.create(item_id: item.id, user_id: user.id)
+
+    Address.create(postal: postal, prefecture_id: prefecture_id, city: city, address1: address1,address2: address2, telephone: telephone, item_id: item_id, user_id: user_id )
+    Order.create(item_id: item_id, user_id: user_id)
   end
 end
