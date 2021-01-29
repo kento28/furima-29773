fab05e3342460482d99e4c7cdd1601edd4186750
kento28 <atnorisiro@gmail.com>
2021-01-29 17:18:06 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,10 +5,6 @@ class Users::SessionsController < Devise::SessionsController
     user = User.guest
     sign_in user
     redirect_to root_path
-    # , notice: 'ゲストユーザーとしてログインしました'
   end
 
-  # def after_sign_in_path_for(_resource)
-  #   root_path
-  # end
 end
